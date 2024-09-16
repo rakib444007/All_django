8dae7113fa59e528ff1cf73d92c29a845165f734
rakib444007 <rh444007@gmail.com>
2024-09-16 18:25:21 +0600

    second app added

--- pro1/urls.py
+++ pro1/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/',views.contact),
-    path('first_app/',include('first_app.urls'))
+    path('first_app/',include('first_app.urls')),
+    # path('second_app/',include('second_app.urls')),
 ]
